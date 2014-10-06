9e902f3091c8546521bf6574a86d95ee7609fbf1
Eric Johnson <eric.johnson12321@gmail.com>
2014-10-06 11:02:28 -0400

    Author
    Added names

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -7,7 +7,7 @@
  * 		    Handles responding to simple words and phrases 
  * </li></ul>
  * This version uses a nested if to handle default responses.
- * @author Laurie White
+ * @author Laurie White, Eric Johnson, Jasjit Janda
  * @version April 2012
  */
 public class Magpie2
